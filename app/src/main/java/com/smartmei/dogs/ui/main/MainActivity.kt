9e1968dd64cfdcd8bfd96ff3b25b167b8da5a3fb
Michael Costa de Castro <cosmichael@gmail.com>
2019-11-09 00:23:24 -0300

    final commit

--- app/src/main/java/com/smartmei/dogs/ui/main/MainActivity.kt
+++ app/src/main/java/com/smartmei/dogs/ui/main/MainActivity.kt
@@ -3,7 +3,6 @@ package com.smartmei.dogs.ui.main
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.smartmei.dogs.ApplicationApp
